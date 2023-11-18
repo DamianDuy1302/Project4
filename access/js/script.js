b2455bf58119af167b1ff818354c6d6516d43043
DamianDuy1302 <damianduy1302@gmail.com>
2023-11-18 14:53:15 +0700

    18/11/23

---
+++ access/js/script.js
@@ -0,0 +1,35 @@
+const navIconOpen = document.querySelector(".navigation__menuIconOpen");
+const navIconClose = document.querySelector(".navigation__menuIconClose");
+console.log(navIconOpen);
+console.log(navIconClose);
+navIconOpen.addEventListener("click", function(){
+    const nav_menu = document.querySelector(".navigation__menu");
+    const nav_content = document.querySelector(".navigation__content");
+    nav_content.style.borderRadius = "45px";
+    nav_content.style.padding = "30px 30px 10px 30px";
+    nav_menu.style.display = "block";
+    navIconOpen.classList.add("hidden");
+    navIconClose.classList.remove("hidden");
+})
+navIconClose.addEventListener("click", function(){
+    const nav_menu = document.querySelector(".navigation__menu");
+    const nav_content = document.querySelector(".navigation__content");
+    nav_content.style.padding = "30px 30px 30px 30px";
+    nav_menu.style.display = "none";
+    navIconOpen.classList.remove("hidden");
+    navIconClose.classList.add("hidden");
+})
+
+
+
+const menu_sections = document.querySelectorAll(".navigation__section");
+console.log(menu_sections)
+menu_sections.forEach(function(item){
+    item.addEventListener("click", function(){
+        const current_section  = document.querySelector(".navigation__section--active")
+        current_section.classList.remove("navigation__section--active");
+        this.classList.add("navigation__section--active");
+    })
+})
+
+
